01b899b65447a38ffaed34b01b7b46ecfeb16e57
CodeZombie <CodeZombie@DESKTOP-TA6ICMU>
2023-08-28 17:58:42 -0300

    Conexão com o banco de dados e GET na tabela Personalidades

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Yurisbk/go-rest-api.git/database"
 	"github.com/Yurisbk/go-rest-api.git/models"
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +16,17 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var personalidades []models.Personalidade
+	database.DB.Find(&personalidades)
+	json.NewEncoder(w).Encode(personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var personalidades models.Personalidade
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	database.DB.First(&personalidades, id)
+	json.NewEncoder(w).Encode(personalidades)
+
 }
--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/Yurisbk/go-rest-api.git/models"
+	"github.com/Yurisbk/go-rest-api.git/database"
 	"github.com/Yurisbk/go-rest-api.git/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Pesonalidade{
-		{Nome: "Nome 1", Historia: "Historia 1"},
-		{Nome: "Nome 2", Historia: "Historia 2"},
-	}
-	fmt.Println("Iniciando seridor Rest com Go")
+	database.ConectaComBancoDeDados()
+	fmt.Println("Iniciando servidor Rest com Go")
 	routes.HandleRequest()
 }
